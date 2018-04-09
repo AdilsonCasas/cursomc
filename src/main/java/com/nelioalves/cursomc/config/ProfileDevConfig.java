b97bc1c97111e7d1c85c50fb82c32e424eae55b3
AdilsonCasas <adilson.casas@gmail.com>
2018-04-09 10:47:32 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/nelioalves/cursomc/config/ProfileDevConfig.java
@@ -0,0 +1,36 @@
+package com.nelioalves.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nelioalves.cursomc.services.DBService;
+
+@Configuration
+// as definições dentro desta classe só terão efeito dentro do profile "test", veja o arq "application-test.properties"
+@Profile("dev")
+public class ProfileDevConfig {
+
+	@Autowired
+	private DBService serviceDB;
+
+	// Esta anotação '@Value' abaixo pega o valor da chave 'spring.jpa.hibernate.ddl-auto' definida no arquivo 'application-dev.properties' para verificar se deve ou não criar a base de dados no BD mysql
+	// coloque ---> 'spring.jpa.hibernate.ddl-auto=create' no arq config se quiser CRIAR o BD ao inicializar esta aplicação ou 'none' no lugar de create para não fazer nada - não criar o BD
+	// o valor da variavel de configuração será colocada dentro da variável local tipo String logo abaixo (no meu caso 'strategy').
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy; // 'estratégia' de criação do BD
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!"create".equals(strategy)) { // qualquer coisa diferente de "create" --> return.
+			return false;
+		}
+		serviceDB.instantiateTestDatabase();
+		return true;
+	}
+
+}
