0b1e52d6e6ba6d9ab63dd435861f6403b87c1cf2
AdilsonCasas <adilson.casas@gmail.com>
2018-04-09 14:26:21 -0300

    Configuração e envio do projeto para produção

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	// no heroku.com o curso se chama "curso-spring-ionic-adilson"
+	// para logar-se no heroku execute no terminal (dentro da pasta do git do projeto): 'heroku login' (ele vai solicitar email+senha de login)
+	// dentro da pasta do git para o projeto execute: 'heroku git:remote -a curso-spring-ionic-adilson'
+	// o comando 'heroku config | grep CLEARDB_DATABASE_URL' fornece os dados remotos do bd mysql
+	//   retornará algo assim: 'mysql://b5a5c5728a3d60:6b723d9e@us-cdbr-iron-east-05.cleardb.net/heroku_5e5e15448243a6c?reconnect=true'
+	//   que indica: 'mysql://usuário:senha@host/url_do_servidor
+	//   para usar no comando pra exportar a base de dados para dentro do heroku: '/opt/lampp/bin/mysql --host=us-cdbr-iron-east-05.cleardb.net --user=b5a5c5728a3d60 --password=6b723d9e --reconnect heroku_5e5e15448243a6c < /home/lenovo/workspace/heroku/curso_spring.sql'
+	// meu link GitHub do curso no Heroku: 'https://git.heroku.com/curso-spring-ionic-adilson.git'
+	// link da minha aplicação no Heroku: 'https://curso-spring-ionic-adilson.herokuapp.com/'
+	// no github.com minha área é 'AdilsonCasas'
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
